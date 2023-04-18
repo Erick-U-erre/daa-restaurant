736b3038da7f29fe1fe30b6283a4c5b7ad25b975
Erick-Flores <erick.flores@u-erre.mx>
2023-04-18 15:08:01 -0600

    creación de menú de platillos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCard, MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -23,6 +24,7 @@ import { ReservationComponent } from './reservation/reservation.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  menuList = [
+    {
+      title: 'Arroz a la Mexicana',
+      image: '../../assets/menu/platillo1.png',
+      description: 'Este platillo está hecho con arroz cocido en un caldo de tomate y cebolla, y se sirve con granos de maíz y chiles picados para darle un toque picante. '
+    },
+    {
+      title: 'Enchiladas Verdes',
+      image: '../../assets/menu/platillo2.png',
+      description: 'Tortillas suaves rellenas de pollo desmenuzado, cubiertas con una salsa de chiles verdes y queso derretido, y acompañadas con frijoles refritos y arroz.'
+    },
+    {
+      title: 'Tacos de Carne Asada con Guacamole',
+      image: '../../assets/menu/platillo3.png',
+      description: 'Tiras de carne asada a la parrilla servidas en tortillas calientes, acompañadas de guacamole fresco hecho con aguacate, tomate y cebolla picados.'
+    },
+    {
+      title: 'Hamburguesa con Queso y Macarrones con Queso',
+      image: '../../assets/menu/platillo4.png',
+      description: 'Una jugosa hamburguesa de carne de res a la parrilla, cubierta con queso cheddar derretido y servida con macarrones con queso cremosos.'
+    },
+    {
+      title: 'Tarta de Fresas con Crema',
+      image: '../../assets/menu/platillo5.png',
+      description: 'Una base de masa quebrada crujiente rellena con una mezcla de fresas frescas, crema pastelera suave y nata montada, coronada con fresas frescas en rodajas. '
+    },
+  ];
 }
